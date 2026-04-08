2d17d13a75187fb6e9e5753a46e46ec7136d3d9f
TMR-kevin <kyoutae.na@tmr-founders.com>
2026-04-08 14:37:02 +0900

    CORS 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://baby-rang.spectrify.kr',
+      'https://baby-rang-admin.spectrify.kr',
+      'http://localhost:13000',
+      'http://localhost:18000',
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 18080);
 }
 bootstrap();
